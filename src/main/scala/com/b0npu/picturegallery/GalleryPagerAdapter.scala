4e1954b135c03d78f34ac4e2200fefd004c1c55d
b0mpu <b0npu@outlook.com>
2016-10-03 20:09:43 +0900

    Implement view GalleryPager with read external storage

--- src/main/scala/com/b0npu/picturegallery/GalleryPagerAdapter.scala
+++ src/main/scala/com/b0npu/picturegallery/GalleryPagerAdapter.scala
@@ -1,28 +1,42 @@
 package com.b0npu.picturegallery
 
-import android.content.Context
+import android.content.{ContentResolver, Context}
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
 import android.support.v4.view.PagerAdapter
 import android.view.{View, ViewGroup}
 import android.widget.ImageView
 
+/**
+  * 画像を表示する
+  *
+  * @param context gallery context
+  */
 class GalleryPagerAdapter(context: Context) extends PagerAdapter {
 
+  /* コンテキスト */
   val galleryContext: Context = context
 
-  var galleryArray: Array[Int] = Array.empty
+  /* SDカードの画像データのURIに問い合わせをして検索結果をCursorに格納する */
+  val imageMediaStoreUri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+  val mediaContentResolver: ContentResolver = galleryContext.getContentResolver
 
-  def addItem(id: Int): Unit = {
-    galleryArray :+= id
+  var galleryArray: Array[Bitmap] = Array.empty
+
+  def addPicture(bitmap: Bitmap): Unit = {
+    galleryArray :+= bitmap
   }
 
   override def instantiateItem(container: ViewGroup, position: Int): AnyRef = {
 
-    val id = galleryArray(position)
+    val bitmapPicture = galleryArray(position)
 
     val imageView: ImageView = new ImageView(galleryContext)
-    imageView.setImageResource(id)
 
+    imageView.setImageBitmap(bitmapPicture)
     container.addView(imageView)
+
     imageView
   }
 
